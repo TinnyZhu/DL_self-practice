193d3d6a0246ec4b3f7a026da974e3265b695255
TinnyZhu <tinzhu@ucdavis.edu>
2020-09-06 17:06:45 -0700

    Step information update

--- Deterministic_MDP.py
+++ Deterministic_MDP.py
@@ -17,7 +17,10 @@ class Motion:
 			self.rewards['w']=-10
 			self.row=self.start_row
 		self.upvalue=self.rewards['w']+self.discount*self.value[self.row][self.column]
-		return self.upvalue
+		self.start_row=self.row
+		self.start_column=self.column
+		self.new_input={'row':self.start_row,'column':self.start_column}
+		return self.upvalue,self.new_input
 
 	def down_value(self,):
 		self.row+=1
@@ -25,7 +28,10 @@ class Motion:
 			self.rewards['s']=-10
 			self.row=self.start_row
 		self.downvalue=self.rewards['s']+self.discount*self.value[self.row][self.column]
-		return self.downvalue
+		self.start_row=self.row
+		self.start_column=self.column
+		self.new_input={'row':self.start_row,'column':self.start_column}
+		return self.downvalue,self.new_input
 
 	def left_value(self,):
 		self.column-=1
@@ -33,7 +39,10 @@ class Motion:
 			self.rewards['a']=-10
 			self.column=self.start_column
 		self.leftvalue=self.rewards['a']+self.discount*self.value[self.row][self.column]
-		return self.leftvalue
+		self.start_row=self.row
+		self.start_column=self.column
+		self.new_input={'row':self.start_row,'column':self.start_column}
+		return self.leftvalue,self.new_input
 
 	def right_value(self,):
 		self.column+=1
@@ -41,7 +50,10 @@ class Motion:
 			self.rewards['d']=-10
 			self.column=self.start_column
 		self.rightvalue=self.rewards['d']+self.discount*self.value[self.row][self.column]
-		return self.rightvalue
+		self.start_row=self.row
+		self.start_column=self.column
+		self.new_input={'row':self.start_row,'column':self.start_column}
+		return self.rightvalue,self.new_input
 
 
 
@@ -51,8 +63,11 @@ def step_optimization(start_row,start_column,value_map,discount,dp):
     
     Step_motion=Motion(start_row,start_column,value_map,discount)
     upvalue=Step_motion.up_value()
+    Step_motion=Motion(start_row,start_column,value_map,discount)
     downvalue=Step_motion.down_value()
+    Step_motion=Motion(start_row,start_column,value_map,discount)
     leftvalue=Step_motion.left_value()
+    Step_motion=Motion(start_row,start_column,value_map,discount)
     rightvalue=Step_motion.right_value()
     
     policy_value={'up':upvalue,'down':downvalue,'left':leftvalue,'right':rightvalue}
